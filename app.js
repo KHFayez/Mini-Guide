c25a788996d3ad15abf2c6f59c21d5b94b6d2bed
KHFayez <89240255+KHFayez@users.noreply.github.com>
2022-01-03 23:03:23 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var toggle_btn;
 var big_wrapper;
 var hamburger_menu;
 
+
 function declare(){
 	toggle_btn = document.querySelector(".toggle-btn");
 	big_wrapper = document.querySelector(".big-wrapper");
@@ -49,3 +50,30 @@ function events() {
 }
 
 events();
+
+function toggleAnimation() {
+	// Clone the wrapper
+	dark = !dark;
+	let clone = big_wrapper.cloneNode(true);
+	if(dark){
+		clone.classList.remove("light");
+		clone.classList.add("dark");
+	} else{
+		clone.classList.remove("dark");
+		clone.classList.add("light");
+	}
+	clone.classList.add("copy");
+	main.appendChild(clone);
+
+	document.body.classList.add("stop-scrolling");
+
+	clone.addEventListener("animationend", () => {
+		document.body.classList.remove("stop-scrolling");
+		big_wrapper.remove();
+		clone.classList.remove("copy");
+		// Reset Variables
+		declare();
+		events();
+	});
+
+}
