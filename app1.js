58abce20d4a56be43ce19482691c69a2f31798a3
KHFayez <89240255+KHFayez@users.noreply.github.com>
2022-01-04 11:26:18 +0200

    Add files via upload

--- app1.js
+++ app1.js
@@ -9,3 +9,52 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+
+
+
+
+function login() {
+
+	firebase.auth() .onAuthStateChanged(function(user){
+	if (user){
+		//user is signed in.
+		
+		document.getElementById("user_div").style.display = "block";
+		document.getElementById("login_div").style.display = "none";
+
+
+		var user = firebase.auth().currentUser;
+
+		if (user != null){
+			var email_id = user.email;
+			document.getElementById("user_para").innerHTML = "Welcome User : " +email_id
+		}
+
+	}
+	else{
+		//No user is signed in.
+
+		document.getElementById("user_div").style.display = "none";
+		document.getElementById("login_div").style.display = "block";
+	}
+});
+
+	var userEmail = document.getElementById("email_field").value;
+	var userPass = document.getElementById("password_field").value;
+
+firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+		//handle Errors here.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+
+		window.alert("Error : " + errorMessage);
+		//...
+});
+
+}
+
+function logout(){
+	firebase.auth().signOut();
+	}
+}
